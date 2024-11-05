92fdba6f1cd9f9640589180531fdc9cb666620b3
saratoga <hunaki130@gmail.com>
2024-11-05 11:10:04 +0900

    環境作成

---
+++ src/main/java/saratoga/gptalkerMC/GptalkerMC.java
@@ -0,0 +1,17 @@
+package saratoga.gptalkerMC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GptalkerMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
